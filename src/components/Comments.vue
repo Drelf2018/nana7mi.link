ca76b6db8753e31aaa73b94b046c37a55ab8a445
Drelf2018 <drelf2018@outlook.com>
2023-02-21 14:26:24 +0800

    fix: 增加行距

--- src/components/Comments.vue
+++ src/components/Comments.vue
@@ -1,6 +1,6 @@
 <template>
     <Comments v-if="comments.repost" :comments="comments.repost" />
-    <Card :card="comments.card" :class="{reply: comments.repost}" style="font-size: 30px;">
+    <Card :card="comments.card" :class="{reply: comments.repost}" style="font-size: 30px;margin: 0.25em 0">
         <span v-html="comments.text" class="comment" :title="comments.date + ' ' + comments.source"></span>
         <a v-if="comments.picUrls[0]" :href="comments.picUrls[0]" class="comment" target="_blank">查看图片</a>
     </Card>
