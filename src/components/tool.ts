058ec870b4733a677163449afa8959e2fae5c74d
Drelf2018 <drelf2018@outlook.com>
2023-02-21 14:27:16 +0800

    feat: 新增博文通知

--- src/components/tool.ts
+++ src/components/tool.ts
@@ -1,9 +1,9 @@
 let api = window.location.search.replace("?source=", "")
 export const ApiUrl = api == "" ? "https://api.nana7mi.link" : api
 
-export function throttle (fn: Function, delay=500) {
+export function throttle(fn: Function, delay = 500) {
     var flag = new Date().getTime();
-    return function(...args) {
+    return function (...args) {
         var tt = new Date().getTime();
         if (tt - flag >= delay) {
             fn.call(this, ...args);
@@ -12,12 +12,37 @@ export function throttle (fn: Function, delay=500) {
     }
 }
 
-export function debounce (fn: Function, delay=1000) {
+export function debounce(fn: Function, delay = 1000) {
     var timer = null;
-    return function(...args) {
+    return function (...args) {
         if (timer) clearTimeout(timer);
-        timer = setTimeout(()=>fn.call(this, ...args), delay);
+        timer = setTimeout(() => fn.call(this, ...args), delay);
     }
+}
+
+// 通知博文
+export function NoticePost(post) {
+    Notification.requestPermission().then(result => {
+        if (result === 'denied') {
+            console.log('Permission wasn\'t granted. Allow a retry.');
+            return;
+        }
+        if (result === 'default') {
+            console.log('The permission request was dismissed.');
+            return;
+        }
+
+        let notice = new Notification("你关注的用户发布了新动态！", {
+            body: `${post.name}：${post.text.replace(/<[^>]*>/g, '')}`,
+            icon: "/favicon.ico",
+            image: post.picUrls[0],
+            data: "https://nana7mi.link"
+        })
+        notice.onclick = function (event) {
+            event.preventDefault(); // prevent the browser from focusing the Notification's tab
+            window.open(post.url, '_blank');
+        }
+    })
 }
 
 export class Theme {
@@ -28,10 +53,10 @@ export class Theme {
     zoom: number
     constructor() {
         this.app = document.getElementById("app")
-        if(document.body.offsetWidth < 750) {
+        if (document.body.offsetWidth < 750) {
             this.app.classList.add("mobile")
             let timeNow = new Date().getHours()
-            if(timeNow >= 21 || timeNow < 6){
+            if (timeNow >= 21 || timeNow < 6) {
                 this.theme = "dark"
             } else {
                 this.theme = "light"
@@ -40,13 +65,13 @@ export class Theme {
             this.app.classList.add("pc")
             this.theme = localStorage.getItem("theme") || "light"
         }
-        
+
         this.setBackgroundColor()
 
         this.setZoom()
         window.onresize = () => this.setZoom()
     }
-    setZoom () {
+    setZoom() {
         this.zoom = 0.8 * document.body.clientWidth / 1200
         this.app.style.setProperty("--zoom", this.zoom.toString())
     }
@@ -73,7 +98,7 @@ export interface faceInfo {
     pendant_color: string
 }
 
-export interface userInfo extends faceInfo{
+export interface userInfo extends faceInfo {
     cover_href: string
     cover_url: string
     title: string
